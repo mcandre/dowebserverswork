953be1c26c33943c5fd8f1f0009f429667858b21
Andrew Pennebaker <Andrew Pennebaker>
2011-03-10 17:57:01 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Do Web Servers Work?</title>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+	<link rel="favorites icon" href="/favicon.ico" />
+</head>
+<body style="text-align: center;">
+<p><a href="https://github.com/mcandre/dowebserverswork">GitHub</a></p>
+<h1>Do Web Servers Work?</h1>
+<table style="margin-left: auto; margin-right: auto; text-align: left;">
+<tr>
+<th>PHP</th><td>...</td>
+</tr>
+<tr>
+<th>Hunchentoot</th><td>...</td>
+</tr>
+<tr>
+<th>Happstack</th><td>...</td>
+</tr>
+</table>
+</body>
+</html>
